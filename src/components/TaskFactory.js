39a444cca7c56cac2b74a2ede124e9a226d7a611
Tiffany Gill <tgill007@gmail.com>
2025-03-20 19:21:37 -0400

    Changed using Date for generating Task id and now using Cyrptro randon.

--- src/components/TaskFactory.js
+++ src/components/TaskFactory.js
@@ -2,7 +2,7 @@
 export class TaskFactory {
   static createTask(title, description, owner, status = "To Do") {
     return {
-      id: Date.now(),
+      id: `${Date.now()}-${Math.floor(Math.random() * 10000)}`, // Adds randomness
       title,
       description,
       owner,
